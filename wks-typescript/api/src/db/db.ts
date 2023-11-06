0c6bd50addd2e92a122decc970ea296855351f55
Rober <roberdz30@gmail.com>
2023-11-06 11:13:06 -0300

    Continue with models

---
+++ wks-typescript/api/src/db/db.ts
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize-typescript"
+import config from "../lib/config"
+config;
+export const sequelize = new Sequelize({
+    dialect: "postgres",
+    database: config.dbName,
+    password: config.dbPassword, 
+
+})
